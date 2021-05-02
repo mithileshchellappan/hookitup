b8e7340efee039abdffb561e8f0b7c87b9c242f0
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-02 12:24:54 +0530

    expands

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -8,19 +8,20 @@ const Accordion = ({ items }) => {
     setActiveIndex(index)
 };
   const itemsRendered = items.map((item, index) => {
+      const active = index === activeIndex ?'active':'';
     return (
       <React.Fragment key={item.title}>
-        <div className="title active" onClick={() => onTitleClick(index )}>
+        <div className={`title ${active}`} onClick={() => onTitleClick(index )}>
           <i className="dropdown icon"></i>
           {item.title}
         </div>
-        <div className="content active">
+        <div className={`content ${active}`}>
           <p>{item.content}</p>
         </div>
       </React.Fragment>
     );
   });
-  return <div className="ui styled accordion">{itemsRendered}<h1>{activeIndex}</h1></div>;
+  return <div className="ui styled accordion">{itemsRendered}</div>;
 };
 
 export default Accordion;
