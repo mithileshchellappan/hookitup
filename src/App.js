84c266a168a8f3286ffb791a2b48efc1f4b01f34
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-08 16:59:49 +0530

    back

--- src/App.js
+++ src/App.js
@@ -1,54 +1,13 @@
 import React, { useState } from "react";
-// import Accordion from "./components/accordion";
-import Dropdown from "./components/Dropdown";
-// import Search from "./components/search";
+import Translate from "./components/translate";
 
-// const items = [
-//   {
-//     title: "React?",
-//     content: "Frontend",
-//   },
-//   {
-//     title: "Node?",
-//     content: "JS",
-//   },
-//   {
-//     title: "Kubernets?",
-//     content: "Docers",
-//   },
-//   {
-//     title: "Docker?",
-//     content: "Dock",
-//   },
-// ];
-const options = [
-  {
-    label: "Red",
-    value: "red",
-  },
-  {
-    label: "Green",
-    value: "green",
-  },
-  {
-    label: "Blue",
-    value: "blue",
-  },
-];
-//<Accordion items={items}/>
 const App = () => {
-  const [selected, setSelected] = useState(options[0]);
-  const [showDropDown, setDropDown] = useState(true);
   return (
     <div>
-      <button onClick={() => setDropDown(!showDropDown)}>Toggle</button>
       {
-        showDropDown?
-        <Dropdown
-        options={options}
-        selected={selected}
-        onSelectedChange={setSelected}
-      />:null}
+        
+        <Translate/>
+        }
     </div>
   );
 };
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -34,7 +34,7 @@ const Dropdown = ({ options, selected, onSelectedChange }) => {
   return (
     <div ref={ref} className="ui form">
       <div className="field">
-        <label className="label">Select a color</label>
+        <label className="label">Select a language</label>
         <div
           onClick={() => setOpen(!open)}
           className={`ui selection dropdown ${open ? "visible active" : null}`}
