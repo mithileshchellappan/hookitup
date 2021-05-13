880250876d72dee03a876ae032efc8e8fab65bd4
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-13 14:26:27 +0530

    payload fetch

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -3,11 +3,16 @@ import {connect} from 'react-redux'
 import {fetchPosts} from '../actions'
 class PostList extends React.Component {
     componentDidMount(){
-        // this.props.fetchPosts()
+        this.props.fetchPosts()
     }
   render() {
+    console.log(this.props.posts)
     return <div>Post List</div>;
   }
 }
 
-export default connect(null,{fetchPosts})(PostList);
+const mapStateToProps=(state)=>{
+  return {posts:state.posts};
+}
+
+export default connect(mapStateToProps,{fetchPosts})(PostList);
