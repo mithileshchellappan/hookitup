e7cd9bfd9d62ef586755f0e0af7ebd9873b5c7d5
mithileshchellappan <35962266+mithileshchellappan@users.noreply.github.com>
2021-05-13 23:42:37 +0530

    generic routing

---
+++ client/src/components/header.js
@@ -0,0 +1,17 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+const Header = () => {
+  return (
+    <div className="ui secondary pointing menu">
+        <Link to="/" className="item">Mr.Streamer</Link>
+      <div className="right menu">
+        <Link to="/" className="item">
+            Streams
+        </Link>
+
+      </div>
+    </div>
+  );
+};
+
+export default Header;
